3e5fbe6ed36d8ffe9f979ea14f05601ebff97569
tonousa <tonousa@hotmail.com>
2017-07-06 09:01:57 -0400

    Start

---
+++ BookService/Scripts/app.js
@@ -0,0 +1,36 @@
+﻿var ViewModel = function () {
+    var self = this;
+    self.books = ko.observableArray();
+    self.error = ko.observable();
+    self.detail = ko.observable();
+
+    var booksUri = "/api/books/";
+
+    function ajaxHelper(uri, method, data) {
+        self.error('');
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            self.error(errorThrown);
+        });
+    }
+
+    function getAllBooks() {
+        ajaxHelper(booksUri, 'GET').done(function (data) {
+            self.books(data);
+        });
+    }
+
+    self.getBookDetail = function (item) {
+        ajaxHelper(booksUri + item.Id, 'GET').done(function (data) {
+            self.detail(data)});
+    };
+
+    getAllBooks();
+};
+
+ko.applyBindings(new ViewModel());
